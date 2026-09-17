4d67ce25282339e26b4bcadef20a31697b681e16
CatWindow <catwindow@meow.org>
2026-09-17 17:01:45 +0300

    windows utf8 (?)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
+#include "define.h"
 #include <iostream>
 #include <string>
+
+#if defined(PLAT_WINDOWS)
+    #include <windows.h>
+#endif
 
 void drawCat();
 
 int main(int argc, char* argv[]) {
+    #if defined(PLAT_WINDOWS)
+        SetConsoleOutputCP(CP_UTF8);
+        SetConsoleCP(CP_UTF8);
+    #endif
+
     bool needToDrawCat = false;
 
     for (int i = 0; i < argc; ++i)
